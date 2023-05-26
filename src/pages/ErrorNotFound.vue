3c048fd2bf6e791480cfea2f53603f471364dbfa
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-26 17:18:40 +0600

    refactor: not found page

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,16 +1,19 @@
 <template>
   <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+    class="fullscreen bg-green-5 text-white text-center q-pa-md flex flex-center"
   >
     <div>
       <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <div class="text-h2" style="opacity: 0.4">
+        Oops. Nothing here...
+        <q-icon name="sports_soccer" class="ball" />
+      </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="green-5"
         unelevated
         to="/"
         label="Go Home"
@@ -22,4 +25,18 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@keyframes ball {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.ball {
+  transform: rotate(0);
+  animation: ball 3s linear infinite;
+}
+</style>
