48c61cb64b03ae4967ed6e63e5669641272416cd
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-28 15:19:05 +0600

    feaT: add request interseptors

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,44 @@
 import axios from "axios";
 import TokenService from "./token";
-
-const token = TokenService.get();
+import router from "src/router";
+import { notify } from "src/boot/notify";
 
 const api = axios.create({
   baseURL: "http://footprotest-001-site1.htempurl.com",
   headers: {
     "Content-Type": "application/json",
-    Accept: "application/json",
-    Authorization: `Bearer ${token || ""}`
+    Accept: "application/json"
   }
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = TokenService.get();
+
+    if (!token) {
+      config.headers.Authorization = undefined;
+    } else {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      notify.show("Autorize error!", "negative");
+      router.push("/auth");
+      TokenService.remove();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
