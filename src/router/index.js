9498701ccdb2bc6e5c8e9237f4138876733f2c22
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-28 11:01:31 +0600

    refactor: router file

--- src/router/index.js
+++ src/router/index.js
@@ -7,19 +7,17 @@ import {
 } from "vue-router";
 import routes from "./routes";
 
-export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === "history"
-    ? createWebHistory
-    : createWebHashHistory;
+const createHistory = process.env.SERVER
+  ? createMemoryHistory
+  : process.env.VUE_ROUTER_MODE === "history"
+  ? createWebHistory
+  : createWebHashHistory;
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
+const router = createRouter({
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+  routes,
 
-    history: createHistory(process.env.VUE_ROUTER_BASE)
-  });
-
-  return Router;
+  history: createHistory(process.env.VUE_ROUTER_BASE)
 });
+
+export default router;
