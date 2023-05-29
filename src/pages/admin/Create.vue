312b62b422042217f92e2fa77ec7ef5ea89eb0b9
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-29 12:18:57 +0600

    refactor: super admin create page

--- src/pages/admin/Create.vue
+++ src/pages/admin/Create.vue
@@ -1,7 +1,40 @@
 <template>
-  <q-page class="flex flex-center column">
-    <h4>Create schedule and stadium page</h4>
+  <q-page class="q-pa-md">
+    <main-card class="q-mb-md">
+      <page-title> Create users and stadiums </page-title>
+    </main-card>
+    <main-card>
+      <h6 class="q-mt-auto q-mb-sm text-body1">Create user</h6>
+      <div class="flex gap-5">
+        <q-input
+          borderless
+          color="green-5"
+          bg-color="white"
+          type="email"
+          outlined
+          dense
+          label="Email"
+          v-model="title"
+        />
+        <q-input
+          borderless
+          color="green-5"
+          bg-color="white"
+          type="email"
+          outlined
+          dense
+          label="User name"
+          v-model="title"
+        />
+      </div>
+    </main-card>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import MainCard from "src/components/ui/MainCard.vue";
+import PageTitle from "src/components/ui/PageTitle.vue";
+import { ref } from "vue";
+
+const title = ref("text");
+</script>
