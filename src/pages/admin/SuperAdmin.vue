aa178292a8d214b1beff0ffd2a5524d01740172b
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-29 15:32:08 +0600

    refactor: base table

--- src/pages/admin/SuperAdmin.vue
+++ src/pages/admin/SuperAdmin.vue
@@ -5,7 +5,7 @@
     </main-card>
 
     <main-card v-if="stadiumStore.stadium">
-      <users-table :row="stadiumStore.stadium" :column="column" />
+      <base-table :row="stadiumStore.stadium" :column="column" />
     </main-card>
   </q-page>
 </template>
@@ -13,7 +13,7 @@
 <script setup>
 import PageTitle from "src/components/ui/PageTitle.vue";
 import MainCard from "src/components/ui/MainCard.vue";
-import UsersTable from "src/components/table/UsersTable.vue";
+import BaseTable from "src/components/table/BaseTable.vue";
 import { useStadiumStore } from "src/stores/stadium";
 const stadiumStore = useStadiumStore();
 
--- src/pages/admin/Users.vue
+++ src/pages/admin/Users.vue
@@ -5,7 +5,7 @@
     </main-card>
 
     <main-card v-if="userStore.users">
-      <users-table :row="userStore.users" :column="column" />
+      <base-table :row="userStore.users" :column="column" />
     </main-card>
   </q-page>
 </template>
@@ -13,7 +13,7 @@
 <script setup>
 import PageTitle from "src/components/ui/PageTitle.vue";
 import MainCard from "src/components/ui/MainCard.vue";
-import UsersTable from "src/components/table/UsersTable.vue";
+import BaseTable from "src/components/table/BaseTable.vue";
 import { useUserStore } from "src/stores/user";
 const userStore = useUserStore();
 userStore.getUsers();
