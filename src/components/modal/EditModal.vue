7ced6e66ae39a3adef9245aa275aefce44bca5d8
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-05-30 15:52:56 +0600

    feat: create edit and remove modals

---
+++ src/components/modal/EditModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-dialog :model-value="isEditModalOpen">
+    <q-card class="max-w-450">
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">
+          <slot name="title"></slot>
+        </div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section>
+        <slot name="body" />
+      </q-card-section>
+
+      <q-card-section class="flex justify-end">
+        <slot name="footer" />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+const props = defineProps(["isEditModalOpen"]);
+</script>
---
+++ src/components/modal/RemoveConfirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-dialog :model-value="isRemoveConfirmOpen">
+    <q-card class="max-w-350">
+      <q-card-section class="row items-center q-pb-lg">
+        <div class="text-body1">Вы действительно хотите удалить?</div>
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section class="flex justify-end gap-5">
+        <q-btn no-caps text-color="red-5" v-close-popup> Cancel </q-btn>
+        <q-btn color="red-5" no-caps @click="$emit('removeHandler')">
+          Delete
+        </q-btn>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+const props = defineProps(["isRemoveConfirmOpen"]);
+</script>
