e8743ce1e28b1a885bfe479ba5a5d18414856e38
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-06-15 11:20:53 +0600

    feaT: create help page

--- src/pages/user/Help.vue
+++ src/pages/user/Help.vue
@@ -1,7 +1,77 @@
 <template>
-  <q-page class="flex flex-center column">
-    <h4>Help page</h4>
+  <q-page class="q-pa-md text-body1">
+    <main-card class="q-mb-md">
+      <page-title>Как пользоватся сайтом?</page-title>
+    </main-card>
+    <main-card class="q-mb-xl">
+      <ul class="text-green-5">
+        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+        <li>Aliquam tincidunt mauris eu risus.</li>
+        <li>Vestibulum auctor dapibus neque.</li>
+        <li>Nunc dignissim risus id metus.</li>
+        <li>Cras ornare tristique elit.</li>
+        <li>Vivamus vestibulum ntulla nec ante.</li>
+        <li>Praesent placerat risus quis eros.</li>
+      </ul>
+    </main-card>
+    <main-card class="q-mb-md">
+      <page-title>Как стать нашим клиетом?</page-title>
+    </main-card>
+    <main-card class="q-mb-xl">
+      <ul class="text-green-5">
+        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+        <li>Aliquam tincidunt mauris eu risus.</li>
+        <li>Vestibulum auctor dapibus neque.</li>
+        <li>Nunc dignissim risus id metus.</li>
+        <li>Cras ornare tristique elit.</li>
+        <li>Vivamus vestibulum ntulla nec ante.</li>
+        <li>Praesent placerat risus quis eros.</li>
+      </ul>
+    </main-card>
+    <main-card class="q-mb-md">
+      <page-title>Наша команда</page-title>
+    </main-card>
+    <main-card>
+      <div class="flex justify-evenly">
+        <div class="text-center text-green-5 text-weight-medium">
+          <q-avatar
+            class="q-mb-md"
+            size="100px"
+            font-size="52px"
+            color="green-5"
+            text-color="white"
+            icon="person"
+          />
+          <div>Mukhammadyorkhon</div>
+        </div>
+        <div class="text-center text-green-5 text-weight-medium">
+          <q-avatar
+            class="q-mb-md"
+            size="100px"
+            font-size="52px"
+            color="green-5"
+            text-color="white"
+            icon="person"
+          />
+          <div>Faizullakhon</div>
+        </div>
+        <div class="text-center text-green-5 text-weight-medium">
+          <q-avatar
+            class="q-mb-md"
+            size="100px"
+            font-size="52px"
+            color="green-5"
+            text-color="white"
+            icon="person"
+          />
+          <div>Kudrattt Halyam Balyam</div>
+        </div>
+      </div>
+    </main-card>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import MainCard from "src/components/ui/MainCard.vue";
+import PageTitle from "src/components/ui/PageTitle.vue";
+</script>
