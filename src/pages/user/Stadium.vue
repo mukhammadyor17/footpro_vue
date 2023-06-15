6cdfdb09264b197f35a8324b1af17bf65d7973a2
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2023-06-15 10:31:18 +0600

    refactor: stadium page

--- src/pages/user/Stadium.vue
+++ src/pages/user/Stadium.vue
@@ -7,7 +7,10 @@
       <base-table :column="col" :row="stadiumStore.updatedStadiums">
         <template v-slot:body-cell-name="{ row }">
           <q-td>
-            <router-link :to="`/stadium/${row.id}`" class="text-black">
+            <router-link
+              :to="`/stadium/${row.id}`"
+              class="text-black full-width"
+            >
               {{ row.name }}
             </router-link>
           </q-td>
@@ -18,11 +21,11 @@
 </template>
 
 <script setup>
-import BaseTable from "src/components/table/BaseTable.vue";
-import MainCard from "src/components/ui/MainCard.vue";
-import PageTitle from "src/components/ui/PageTitle.vue";
 import { stadiumColumn } from "src/constants/columns";
 import { useStadiumStore } from "src/stores/stadium";
+import MainCard from "src/components/ui/MainCard.vue";
+import PageTitle from "src/components/ui/PageTitle.vue";
+import BaseTable from "src/components/table/BaseTable.vue";
 
 const col = stadiumColumn.slice(0, -1);
 const stadiumStore = useStadiumStore();
